c7eb9e862af985ec0e75528fd0f72a6769644572
kwa_laa <khoaddnguyen@gmail.com>
2023-09-20 00:11:23 -0500

    add href and background photos to home, about and contacts

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import requests
+
+
+posts = requests.get("https://api.npoint.io/eb6cd8a5d783f501ee7d").json()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
